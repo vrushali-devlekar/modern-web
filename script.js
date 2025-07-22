cc928597454a4454e4d0fea7ce3e5cf100958303
Vrushali <118001762+vrushali-devlekar@users.noreply.github.com>
2025-07-22 18:15:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+  });
+}
+circleMouseFollower();
